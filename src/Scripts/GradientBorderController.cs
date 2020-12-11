788b3006a4a55366fb8872904d6c6e72ae8efdd1
ark-yugen <jccoker94@gmail.com>
2020-12-10 21:16:44 -0600

    extracted gradient border logic

---
+++ src/Scripts/GradientBorderController.cs
@@ -0,0 +1,317 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Collections;
+using System.Linq;
+
+public class GradientBorderController : MonoBehaviour
+{
+	[SerializeField] private Renderer imageMesh;
+	[SerializeField] private GameObject labelPrefab;
+	[SerializeField] private float mapScale = 100f;
+	[SerializeField] public int MapWidth = 8192;
+	[SerializeField] public int MapHeight = 8192;
+
+	[Header("Image Data")]
+	[SerializeField] public Texture2D ProvinceIdTex;
+	[SerializeField] private Texture2D terrainNormalTex;
+	[SerializeField] private Texture2D lookupTex;
+	[SerializeField] private RenderTexture distanceTransformTex;
+	[SerializeField] private Texture2D colorMapIndirectionTex;
+	[SerializeField] private Texture2D displayedColorMapTex;
+	[SerializeField] private Texture2D biomeColorMapTex;
+
+	[Header("GPU")]
+	[SerializeField] private ComputeShader distanceFieldCS;
+	[SerializeField] private ComputeShader createIndirectionMapCS;
+	[SerializeField] private ComputeShader getTerritoryPixelsCS;
+	[SerializeField] private ComputeShader getProvinceEdgePixelsCS;
+	[SerializeField] private ComputeShader getBorderBitmaskCS;
+
+	[Header("Provinces")]
+	private List<Province> ProvincesInImage;
+
+	[HideInInspector] public Dictionary<Color32, Province> ProvinceColorDict;
+	[HideInInspector] public Dictionary<int, Province> ProvinceIntIDDict;
+	[HideInInspector] public Dictionary<int, Border> BorderIDDict;
+	[HideInInspector] public List<Border> BorderList;
+
+	private RenderTexture A;
+	private RenderTexture B;
+
+	private ComputeBuffer getTerritoryPixelsBuffer;
+	private ComputeBuffer getTerritoryPixelsArgBuffer;
+
+	private ComputeBuffer getEdgePixelsBuffer;
+	private ComputeBuffer getEdgePixelsArgBuffer;
+
+	private void Initialize()
+	{
+		BorderList = new List<Border>();
+
+		//Create a render texture.
+		A = new RenderTexture(ProvinceIdTex.width, ProvinceIdTex.height, 32);
+		A.enableRandomWrite = true;
+		A.filterMode = FilterMode.Point;
+		A.wrapMode = TextureWrapMode.Repeat;
+		A.Create();
+
+		B = new RenderTexture(ProvinceIdTex.width, ProvinceIdTex.height, 32);
+		B.enableRandomWrite = true;
+		B.wrapMode = TextureWrapMode.Repeat;
+		B.filterMode = FilterMode.Point;
+		B.Create();
+
+		distanceTransformTex = new RenderTexture(ProvinceIdTex.width, ProvinceIdTex.height, 32);
+		distanceTransformTex.enableRandomWrite = true;
+		distanceTransformTex.wrapMode = TextureWrapMode.Repeat;
+		distanceTransformTex.filterMode = FilterMode.Bilinear;
+		distanceTransformTex.Create();
+
+		//Load province data
+		if (ProvinceIdTex == null || lookupTex == null)
+		{
+			Debug.LogError("No province data or lookup map loaded in editor!");
+			return;
+		}
+
+		MapWidth = ProvinceIdTex.width;
+		MapHeight = ProvinceIdTex.height;
+
+		getTerritoryPixelsBuffer = new ComputeBuffer(ProvinceIdTex.width * ProvinceIdTex.height, sizeof(float) * 2, ComputeBufferType.Append);
+		getTerritoryPixelsBuffer.SetCounterValue(0);
+
+		getTerritoryPixelsArgBuffer = new ComputeBuffer(4, sizeof(float) * 2, ComputeBufferType.IndirectArguments);
+
+		getEdgePixelsBuffer = new ComputeBuffer(ProvinceIdTex.width * ProvinceIdTex.height, sizeof(float) * 2, ComputeBufferType.Append);
+		getEdgePixelsBuffer.SetCounterValue(0);
+
+		getEdgePixelsArgBuffer = new ComputeBuffer(4, sizeof(float) * 2, ComputeBufferType.IndirectArguments);
+
+		CreateIndirectionTex();
+	}
+
+	private void CreateIndirectionTex()
+	{
+		RenderTexture temp = RenderTexture.GetTemporary(256, 256, 32);
+		temp.enableRandomWrite = true;
+		temp.wrapMode = TextureWrapMode.Clamp;
+		temp.filterMode = FilterMode.Point;
+		temp.Create();
+
+		int createIndirectionCSKernel = createIndirectionMapCS.FindKernel("CSMain");
+		createIndirectionMapCS.SetTexture(createIndirectionCSKernel, "ProvinceMap", ProvinceIdTex);
+		createIndirectionMapCS.SetTexture(createIndirectionCSKernel, "LookupMap", lookupTex);
+		createIndirectionMapCS.SetTexture(createIndirectionCSKernel, "Result", temp);
+		createIndirectionMapCS.Dispatch(createIndirectionCSKernel, ProvinceIdTex.width / 32, ProvinceIdTex.height / 32, 1);
+		GL.Flush();
+
+		colorMapIndirectionTex = new Texture2D(256, 256, TextureFormat.RGBA32, false);
+		colorMapIndirectionTex.wrapMode = TextureWrapMode.Clamp;
+		colorMapIndirectionTex.filterMode = FilterMode.Point;
+
+		RenderTexture.active = temp;
+		colorMapIndirectionTex.ReadPixels(new Rect(0, 0, 256, 256), 0, 0);
+		colorMapIndirectionTex.Apply();
+
+		RenderTexture.active = null;
+		RenderTexture.ReleaseTemporary(temp);
+	}
+
+	private void GenerateDisplayColorTex()
+	{
+		displayedColorMapTex = new Texture2D(256, 256, TextureFormat.RGBA32, false, true);
+		displayedColorMapTex.filterMode = FilterMode.Point;
+
+		int texWidth = displayedColorMapTex.width;
+		int texHeight = displayedColorMapTex.height;
+
+		NativeArray<byte> displayedColB = new NativeArray<byte>(displayedColorMapTex.height * (displayedColorMapTex.width * 4), Allocator.Temp);
+
+		for (int y = 0; y < texHeight; y++)
+		{
+			for (int x = 0; x < texWidth; x++)
+			{
+				int pixelIndex = (x + (texWidth * y)) * 4;
+
+				Color32 currentProvColorId = colorMapIndirectionTex.GetPixel(x, y);
+
+				if (ProvinceColorDict.TryGetValue(currentProvColorId, out Province prov))
+				{
+					Color32 provCol = prov.ColorID;
+
+					displayedColB[pixelIndex] = provCol.r;
+					displayedColB[pixelIndex + 1] = provCol.g;
+					displayedColB[pixelIndex + 2] = provCol.b;
+					displayedColB[pixelIndex + 3] = 255;
+				}
+				else
+				{
+					displayedColB[pixelIndex] = 0;
+					displayedColB[pixelIndex + 1] = 0;
+					displayedColB[pixelIndex + 2] = 0;
+					displayedColB[pixelIndex + 3] = 0;
+				}
+			}
+		}
+
+		displayedColorMapTex.SetPixelData(displayedColB, 0);
+		displayedColorMapTex.Apply();
+
+		displayedColB.Dispose();
+	}
+
+	private void GenerateDistanceTransformMap(Texture2D colorMap)
+	{
+		int initKernel = distanceFieldCS.FindKernel("Init");
+		int jfaKernel = distanceFieldCS.FindKernel("JFA");
+		int renderKernel = distanceFieldCS.FindKernel("CSRender");
+		int blurKernel = distanceFieldCS.FindKernel("CSBlur");
+
+		//Share it with the pixel shader.
+		imageMesh.materials[1].SetTexture("_DistanceTransformMap", distanceTransformTex);
+
+		//Provide the other textures and calculate the render texture with the JFA compute shader.
+		distanceFieldCS.SetFloat("w", A.width);
+		distanceFieldCS.SetFloat("h", A.height);
+		distanceFieldCS.SetFloat("N", 16f); //Basically the max distance a pixel can be from an edge and still update.  22.6274f
+
+		distanceFieldCS.SetTexture(initKernel, "Provinces", ProvinceIdTex);
+		distanceFieldCS.SetTexture(initKernel, "Lookup", lookupTex);
+		distanceFieldCS.SetTexture(initKernel, "Color", colorMap);
+		distanceFieldCS.SetTexture(initKernel, "Out", B);
+
+		distanceFieldCS.Dispatch(initKernel, A.width / 32, A.height / 32, 1);
+
+		//JFA Offset 32
+		distanceFieldCS.SetFloat("StepWidth", 32f);
+		distanceFieldCS.SetTexture(jfaKernel, "In", B);
+		distanceFieldCS.SetTexture(jfaKernel, "Out", A);
+		distanceFieldCS.Dispatch(jfaKernel, A.width / 32, A.height / 32, 1);
+
+		//JFA Offset 16
+		distanceFieldCS.SetFloat("StepWidth", 16f);
+		distanceFieldCS.SetTexture(jfaKernel, "In", A);
+		distanceFieldCS.SetTexture(jfaKernel, "Out", B);
+		distanceFieldCS.Dispatch(jfaKernel, A.width / 32, A.height / 32, 1);
+
+		//JFA Offset 8
+		distanceFieldCS.SetFloat("StepWidth", 8f);
+		distanceFieldCS.SetTexture(jfaKernel, "In", B);
+		distanceFieldCS.SetTexture(jfaKernel, "Out", A);
+		distanceFieldCS.Dispatch(jfaKernel, A.width / 32, A.height / 32, 1);
+
+		//JFA Offset 4
+		distanceFieldCS.SetFloat("StepWidth", 4f);
+		distanceFieldCS.SetTexture(jfaKernel, "In", A);
+		distanceFieldCS.SetTexture(jfaKernel, "Out", B);
+		distanceFieldCS.Dispatch(jfaKernel, A.width / 32, A.height / 32, 1);
+
+		//JFA Offset 2
+		distanceFieldCS.SetFloat("StepWidth", 2f);
+		distanceFieldCS.SetTexture(jfaKernel, "In", B);
+		distanceFieldCS.SetTexture(jfaKernel, "Out", A);
+		distanceFieldCS.Dispatch(jfaKernel, A.width / 32, A.height / 32, 1);
+
+		//JFA Offset 1
+		distanceFieldCS.SetFloat("StepWidth", 1f); //
+		distanceFieldCS.SetTexture(jfaKernel, "In", A);
+		distanceFieldCS.SetTexture(jfaKernel, "Out", B);
+		distanceFieldCS.Dispatch(jfaKernel, A.width / 32, A.height / 32, 1);
+
+		//Render
+		distanceFieldCS.SetTexture(renderKernel, "In", B);
+		distanceFieldCS.SetTexture(renderKernel, "Out", A);
+		distanceFieldCS.Dispatch(renderKernel, A.width / 32, A.height / 32, 1);
+
+		//Blur
+		distanceFieldCS.SetTexture(blurKernel, "In", A);
+		distanceFieldCS.SetTexture(blurKernel, "Out", distanceTransformTex);
+		distanceFieldCS.Dispatch(blurKernel, A.width / 32, A.height / 32, 1);
+	}
+
+	private float2[] GetTerritoryPixels(float[] _searchColFloat4, Texture2D _indirectionMap)
+	{
+		getTerritoryPixelsBuffer.SetCounterValue(0);
+		getTerritoryPixelsCS.SetFloats("SearchColor", _searchColFloat4);
+		getTerritoryPixelsCS.SetTexture(0, "Indirection", _indirectionMap);
+
+		getTerritoryPixelsCS.Dispatch(0, ProvinceIdTex.width / 32, ProvinceIdTex.height / 32, 1);
+
+		int[] args = new int[] { 0, 1, 0, 0 };
+		getTerritoryPixelsArgBuffer.SetData(args);
+		ComputeBuffer.CopyCount(getTerritoryPixelsBuffer, getTerritoryPixelsArgBuffer, 0);
+		getTerritoryPixelsArgBuffer.GetData(args);
+
+		float2[] result = new float2[args[0]];
+		getTerritoryPixelsBuffer.GetData(result, 0, 0, args[0]);
+
+		return result;
+	}
+
+	private (float[], float[]) GetTerritoryPixels(float[] _searchColFloat4, Texture2D _indirectionMap, bool floatTuple = false)
+	{
+		getTerritoryPixelsBuffer.SetCounterValue(0);
+		getTerritoryPixelsCS.SetFloats("SearchColor", _searchColFloat4);
+		getTerritoryPixelsCS.SetTexture(0, "Indirection", _indirectionMap);
+
+		getTerritoryPixelsCS.Dispatch(0, ProvinceIdTex.width / 32, ProvinceIdTex.height / 32, 1);
+
+		int[] args = new int[] { 0, 1, 0, 0 };
+		getTerritoryPixelsArgBuffer.SetData(args);
+		ComputeBuffer.CopyCount(getTerritoryPixelsBuffer, getTerritoryPixelsArgBuffer, 0);
+		getTerritoryPixelsArgBuffer.GetData(args);
+
+		float2[] result = new float2[args[0]];
+		getTerritoryPixelsBuffer.GetData(result, 0, 0, args[0]);
+
+		float[] resultX = result.AsParallel().Select((val) => val.x).ToArray();
+		float[] resultY = result.AsParallel().Select((val) => val.y).ToArray();
+
+		return (resultX, resultY);
+	}
+
+	private (float[], float[]) GetProvinceEdgePixels(float[] _searchColFloat4)
+	{
+		getEdgePixelsBuffer.SetCounterValue(0);
+		getProvinceEdgePixelsCS.SetFloats("SearchColor", _searchColFloat4);
+		getProvinceEdgePixelsCS.SetTexture(0, "ProvinceMap", ProvinceIdTex);
+
+		getProvinceEdgePixelsCS.Dispatch(0, ProvinceIdTex.width / 32, ProvinceIdTex.height / 32, 1);
+
+		int[] args = new int[] { 0, 1, 0, 0 };
+		getEdgePixelsArgBuffer.SetData(args);
+		ComputeBuffer.CopyCount(getEdgePixelsBuffer, getEdgePixelsArgBuffer, 0);
+		getEdgePixelsArgBuffer.GetData(args);
+
+		float2[] result = new float2[args[0]];
+		getEdgePixelsBuffer.GetData(result, 0, 0, args[0]);
+
+		float[] resultX = result.AsParallel().Select((val) => val.x).ToArray();
+		float[] resultY = result.AsParallel().Select((val) => val.y).ToArray();
+
+		return (resultX, resultY);
+	}
+
+	private void GetAllProvincePixels()
+	{
+		for (int i = 1; i < ProvincesInImage.Count; i++)
+		{
+			float[] searchCol = { ProvincesInImage[i].ColorID.r / 255f, ProvincesInImage[i].ColorID.g / 255f, ProvincesInImage[i].ColorID.b / 255f, 1f };
+			(ProvincesInImage[i].PixelsX, ProvincesInImage[i].PixelsY) = GetTerritoryPixels(searchCol, displayedColorMapTex, true);
+			ProvincesInImage[i].PixelSize = ProvincesInImage[i].PixelsX.Length;
+			ProvincesInImage[i].MinXY = new float2(ProvincesInImage[i].PixelsX.AsParallel().Min(), ProvincesInImage[i].PixelsY.AsParallel().Min());
+			ProvincesInImage[i].MaxXY = new float2(ProvincesInImage[i].PixelsX.AsParallel().Max(), ProvincesInImage[i].PixelsY.AsParallel().Max());
+
+			ProvincesInImage[i].PixelsXInt = new int[ProvincesInImage[i].PixelsX.Length];
+			ProvincesInImage[i].PixelsYInt = new int[ProvincesInImage[i].PixelsY.Length];
+
+			int texWidth = ProvinceIdTex.width;
+
+			ProvincesInImage[i].PixelsXInt = ProvincesInImage[i].PixelsX.AsParallel().Select((val) => Mathf.RoundToInt(val * texWidth)).ToArray();
+			ProvincesInImage[i].PixelsYInt = ProvincesInImage[i].PixelsY.AsParallel().Select((val) => Mathf.RoundToInt(val * texWidth)).ToArray();
+		}
+	}
+
+}
---
+++ src/Shaders/MapColorOverlay.shader
@@ -0,0 +1,195 @@
+﻿Shader "PostfallEditor/MapColorOverlay"
+{
+    Properties
+    {
+        _NoiseMap("Noise Map", 2D) = "black" {}
+        _NoiseMultiplier("UV Distortion Strength", Range(0, 0.01)) = 0.005
+        _NoiseFactor("UV Distortion Factor", Range(0, 100)) = 10
+        _DistanceTransformMap ("Distance Field Map", 2D) = "black" {}
+        _NormalMap ("Normal Map", 2D) = "bump" {}
+        _NormalStrength ("Normal Strength", Range(0, 5)) = 1.0
+        _ColorDisplayMap ("Color Display Map", 2D) = "black" {}
+        _LookupMap ("Lookup Map", 2D) = "black" {}
+        _BaseOpacity ("Base Opacity", Range(0,1)) = 0.2
+        _EdgeOpacity ("Edge Opacity", Range(0,1)) = 1.0
+        _BorderPower ("Border Power", Range(0, 1)) = 0.25
+        _BorderReserveEdge ("Border Reserve Edge", Range(0, 1)) = 1.0
+        _BorderReserveFade ("Border Reserve Fade", Range(0, 1)) = 1.0
+        _HardEdgeWidth ("Hard Edge Width", Range(1, 0)) = 0.9
+        _EdgeBlur("Edge Blur", Range(0, 1)) = 0.5
+        _EdgeBlurRadius("Edge Blur Radius", Int) = 1
+        _ColBrightness("Base Brightness", Range(0,1)) = 1
+        _ColSaturation("Base Saturation", Range(0,1)) = 1
+        _ColContrast("Base Contrast", Range(0, 1)) = 1
+        _EdgeBrightness("Edge Brightness", Range(0,5)) = 1
+        _EdgeSaturation("Edge Saturation", Range(0,5)) = 1
+        _EdgeContrast("Edge Contrast", Range(0, 5)) = 1
+        _ColGain ("Gain", Range(0,2)) = 1
+    }
+    SubShader
+    {
+        Tags { "Queue"="Transparent" "RenderType"="Transparent" }
+        LOD 200
+        ZWrite Off
+        Blend SrcAlpha OneMinusSrcAlpha
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows alpha
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+        sampler2D _NoiseMap;
+        sampler2D _LookupMap;
+        sampler2D _ColorDisplayMap;
+        sampler2D _DistanceTransformMap;
+        sampler2D _NormalMap;
+        half _NormalStrength;
+
+        float4 _LookupMap_TexelSize;
+
+        struct Input
+        {
+            float2 uv_LookupMap;
+            float2 uv_ColorMap;
+        };
+
+        half _BaseOpacity;
+        half _EdgeOpacity;
+        half _BorderPower;
+        half _BorderReserveEdge;
+        half _BorderReserveFade;
+        half _HighEndRolloffStrength;
+        half _HardEdgeWidth;
+        half _NoiseMultiplier;
+        half _NoiseFactor;
+
+        half _EdgeBlur;
+        int _EdgeBlurRadius;
+
+        half _EdgeBrightness;
+        half _EdgeSaturation;
+        half _EdgeContrast;
+
+        half _ColBrightness;
+        half _ColSaturation;
+        half _ColContrast;
+        half _ColGain;
+
+        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+        // #pragma instancing_options assumeuniformscaling
+        UNITY_INSTANCING_BUFFER_START(Props)
+            // put more per-instance properties here
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        float map(float OldMin, float OldMax, float NewMin, float NewMax, float OldValue){
+        
+            float OldRange = (OldMax - OldMin);
+            float NewRange = (NewMax - NewMin);
+            float NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
+        
+            return(NewValue);
+        }
+
+        float invLerp(float from, float to, float value) {
+            return (value - from) / (to - from);
+        }
+
+        float4 invLerp(float4 from, float4 to, float4 value) {
+            return (value - from) / (to - from);
+        }
+
+        float3 BrightnessSaturationContrast(float3 color, float brightness, float saturation, float contrast)
+        {
+            // adjust these values to adjust R, G, B colors separately
+            float avgLumR = 0.5;
+            float avgLumG = 0.5;
+            float avgLumB = 0.5;
+
+            // luminance coefficient for getting luminance from the image
+            float3 luminanceCoeff = float3(0.2125, 0.7154, 0.0721);
+
+            // Brightness calculation
+            float3 avgLum = float3(avgLumR, avgLumG, avgLumB);
+            float3 brightnessColor = color * brightness;
+            float intensityf = dot(brightnessColor, luminanceCoeff);
+            float3 intensity = float3(intensityf, intensityf, intensityf);
+
+            // Saturation calculation
+            float3 saturationColor = lerp(intensity, brightnessColor, saturation);
+
+            // Contrast calculation
+            float3 contrastColor = lerp(avgLum, saturationColor, contrast);
+
+            return contrastColor;
+        }
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            float4 noiseSampleA = tex2D(_NoiseMap, (IN.uv_LookupMap * _NoiseFactor));
+            float4 noiseSampleB = tex2D(_NoiseMap, (IN.uv_LookupMap * _NoiseFactor) + 0.1);
+
+            float4 randomValRGBA0 = noiseSampleA - 0.5;
+            float randomVal0 = randomValRGBA0.x * _NoiseMultiplier * 0.5;
+
+            float4 randomValRGBA1 = noiseSampleB - 0.5;
+            float randomVal1 = randomValRGBA1.y * _NoiseMultiplier;
+
+            float2 distortedUV = float2(IN.uv_LookupMap.x + randomVal0, IN.uv_LookupMap.y + randomVal1);
+            
+            //fixed2 lookup = tex2D (_LookupMap, distortedUV).xy + (0.5 * _LookupMap_TexelSize);
+            //float4 col = tex2D(_ColorDisplayMap, lookup);
+
+            float4 c = float4(0,0,0,0);
+
+            float edgeBlurPow = _EdgeBlur * _EdgeBlur;
+
+            int numNeighbors = _EdgeBlurRadius + _EdgeBlurRadius + 1;
+            numNeighbors *= numNeighbors;
+
+            for (int y = -_EdgeBlurRadius; y <= _EdgeBlurRadius; y++) 
+            {
+                for (int x = -_EdgeBlurRadius; x <= _EdgeBlurRadius; x++)
+                {
+                    float2 nUv = float2(distortedUV.x + (x * _LookupMap_TexelSize.x * edgeBlurPow), distortedUV.y + (y * _LookupMap_TexelSize.y * edgeBlurPow));
+                    fixed2 nLookup = tex2D(_LookupMap, nUv).xy;
+                    float4 nC = tex2D(_ColorDisplayMap, nLookup);
+
+                    c += nC;
+                }
+            }
+
+            c /= numNeighbors;
+
+            float4 d = tex2D(_DistanceTransformMap, IN.uv_LookupMap);
+
+            float distPower = pow(d.a, _BorderPower);
+            float reserve = lerp(distPower, d.a, smoothstep(_BorderReserveEdge, _BorderReserveEdge - (_BorderReserveEdge * _BorderReserveFade), d.a));
+            float a = lerp(_BaseOpacity, 1, 1 - reserve) * _ColGain;
+
+            //float edgeOpacity = lerp(0, _BaseOpacity, _EdgeOpacity);
+            float hardEdge = step(d.a, _HardEdgeWidth);
+            a = lerp(a, _EdgeOpacity, hardEdge);
+
+            float4 cEdge = float4(BrightnessSaturationContrast(c.rgb, _EdgeBrightness, _EdgeSaturation, _EdgeContrast), 0);
+            c = lerp(c, cEdge, hardEdge);
+
+            fixed3 normal = UnpackNormal(tex2D(_NormalMap, IN.uv_LookupMap));
+            normal.xy *= _NormalStrength;
+
+            float3 outputCol = BrightnessSaturationContrast(c.rgb, _ColBrightness, _ColSaturation, _ColContrast);
+            float outputAlpha = a;
+    
+            o.Albedo = outputCol;
+            o.Normal = normalize(normal);
+            o.Emission = 0;       
+            o.Metallic = 0;
+            o.Smoothness = 0;
+            o.Alpha = outputAlpha;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
